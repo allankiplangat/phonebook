f8d5531068a2cde4cca83fb0595e17f7551699b7
allankiplangat <allankiplangat22@outlook.com>
2018-03-01 13:08:32 +0300

    Refactored and added  a lookup method

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 """
 Phonebook app that adds, updates, deletes and view contacts
 """
-class PhoneBook:
-    def __init__(self, name, phone_number):
-        self.username = name
-        self.number = phone_number
-        self.contacts = {}
-    def add_contact(self):
-        self.contacts[self.username] = self.number
-        if self.contacts.get(self.username) is None:
-            return False
-
-        return True
+class Phonebook:
+    def __init__(self):
+        self.entries = {}
+    
+    def add(self, name, number):
+        self.entries[name] = number
+    
+    def lookup(self, name):
+        return self.entries[name]
 
     
--- test_app.py
+++ test_app.py
@@ -1,9 +1,12 @@
 import unittest
-import app
+from app import Phonebook
+
 
 class PhonebookTest(unittest.TestCase):
-    def setUp(self):
-        self.phonebook = app.PhoneBook('Allan', '0718150768')
+    def test_create_phonebook(self):
+        phonebook = Phonebook()
 
-    def test_add_contact(self):
-        self.assertTrue(self.phonebook.add_contact())
+    def test_lookup_entry_by_name(self):
+        phonebook = Phonebook()
+        phonebook.add("Naomi", "0834557234")
+        self.assertEqual("0834557234", phonebook.lookup("Naomi"))
